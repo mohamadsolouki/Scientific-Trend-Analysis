942c9cd8dd028a60544054e677c2742bc23b92be
mohamadsolouki <mohamad.solouki@gmail.com>
2023-09-11 02:23:58 +0330

    data samples

--- data/sampling.py
+++ data/sampling.py
@@ -2,7 +2,7 @@ import pandas as pd
 
 # Specify the file paths
 input_file = 'data_preprocessed.csv'
-output_file = 'processed-sample.csv'
+output_file = 'data_processed-sample.csv'
 
 # Read the first 100 rows of the original dataset
 df = pd.read_csv(input_file, nrows=100)
@@ -14,7 +14,7 @@ print(f'Sample processed data saved to {output_file}')
 
 # Specify the file paths
 input_file = 'data_clustered.csv'
-output_file = 'clustered_sample.csv'
+output_file = 'data_clustered_sample.csv'
 
 # Read the first 100 rows of the original dataset
 df = pd.read_csv(input_file, nrows=100)
@@ -23,3 +23,15 @@ df = pd.read_csv(input_file, nrows=100)
 df.to_csv(output_file, index=False)
 
 print(f'Sample clustered data saved to {output_file}')
+
+# Specify the file paths
+input_file = 'data_concatenated.csv'
+output_file = 'data_concatenated_sample.csv'
+
+# Read the first 100 rows of the original dataset
+df = pd.read_csv(input_file, nrows=100)
+
+# Write the sample dataset to a new file
+df.to_csv(output_file, index=False)
+
+print(f'Sample concatenated data saved to {output_file}')
