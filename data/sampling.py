0f71053ac3685df9aefb68959a6434e1ab32ef2a
mohamadsolouki <mohamad.solouki@gmail.com>
2023-09-18 19:17:24 +0330

    update

--- data/sampling.py
+++ data/sampling.py
@@ -1,7 +1,7 @@
 import pandas as pd
 
 # Specify the file paths
-input_file = 'data_preprocessed2.csv'
+input_file = 'data_preprocessed.csv'
 output_file = 'data_preprocessed-sample.csv'
 
 # Read the first 100 rows of the original dataset
